d1052cbc535c1b485872764d52e562498b9c0f18
Gaurav Kispotta <gaurav.ranchi1@gmail.com>
2025-01-09 23:49:20 +0530

    implemented floating edges

--- src/components/nodes/RedisNode.tsx
+++ src/components/nodes/RedisNode.tsx
@@ -8,8 +8,15 @@ function RedisNode() {
       <div>
         <img src='https://www.vectorlogo.zone/logos/redis/redis-icon.svg' />
       </div>
-      <Handle type="source" position={Position.Right} id="a" isConnectable={true} />
-      <Handle type="target" position={Position.Right} id="b" isConnectable={true} />
+      <Handle type="source" position={Position.Top} id="a" isConnectable={true} />
+      <Handle type="source" position={Position.Right} id="b" isConnectable={true} />
+      <Handle type="source" position={Position.Bottom} id="c" isConnectable={true} />
+      <Handle type="source" position={Position.Left} id="d" isConnectable={true} />
+
+      <Handle type="target" position={Position.Top} id="e" isConnectable={true} />
+      <Handle type="target" position={Position.Right} id="f" isConnectable={true} />
+      <Handle type="target" position={Position.Bottom} id="g" isConnectable={true} />
+      <Handle type="target" position={Position.Left} id="h" isConnectable={true} />
     </>
   )
 }
