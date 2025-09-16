42deec30afc387eefda9a7266eff284f2aed66be
Gaurav Kispotta <gaurav.ranchi1@gmail.com>
2025-09-16 15:18:41 +0530

    chore: update version to 0.0.8 and add Firebase integration
    - Bump version in package.json from 0.0.7 to 0.0.8
    - Add Firebase SDK as a dependency
    - Initialize Firebase in main.tsx and set up analytics
    - Log browser device information on initialization

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,33 @@ import App from './App.tsx'
 import 'antd/dist/reset.css'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDqam6NKgdk6q_ES5PqHb_wovTH4wr7vsE",
+  authDomain: "docker-deck.firebaseapp.com",
+  projectId: "docker-deck",
+  storageBucket: "docker-deck.firebasestorage.app",
+  messagingSenderId: "282733946087",
+  appId: "1:282733946087:web:31d4a5b5fda4e375266e42",
+  measurementId: "G-3M0RTM3BBS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+analytics.app.automaticDataCollectionEnabled = true;
+
+logEvent(analytics, 'browser_device', {
+  "browser_agent": navigator.userAgent
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
